0e0593525671db7f34f6abf1029b99972a1e369d
Noah Reeder <noahreederatc@gmail.com>
2024-06-14 00:58:16 -0500

    Added decode_string utility in communication-master-example

--- communication-master-example/Utils.h
+++ communication-master-example/Utils.h
@@ -6,6 +6,7 @@
 #define COMMUNICATION_MASTER_EXAMPLE_UTILS_H
 
 #include <vector>
+#include <string>
 #include <cstdint>
 
 // Packs a 32-bit integer into a vector of 8-bit integers, in little-endian format.
@@ -59,7 +60,8 @@ inline std::vector<uint8_t> firmatify_32(const std::vector<uint8_t>::const_itera
                                 }
     );
 }
-inline std::vector<uint8_t> firmatify_32(const std::vector<uint8_t>& pack) { return firmatify_32(pack.cbegin()); }
+
+inline std::vector<uint8_t> firmatify_32(const std::vector <uint8_t>& pack) { return firmatify_32(pack.cbegin()); }
 
 // Convert a packed byte vector to the 7-bit packets Firmata receives.
 // Useful for checking a == decode_16(firmatify_16(pack_16(a)))
@@ -81,7 +83,19 @@ inline std::vector<uint8_t> firmatify_16(const std::vector<uint8_t>::const_itera
                                 }
     );
 }
-inline std::vector<uint8_t> firmatify_16(const std::vector<uint8_t>& pack) { return firmatify_16(pack.cbegin()); }
+
+inline std::vector<uint8_t> firmatify_16(const std::vector <uint8_t>& pack) { return firmatify_16(pack.cbegin()); }
+
+// Convert a packed byte vector to the 7-bit packets Firmata receives.
+// Useful for checking a == decode_8(firmatify_8(a))
+//
+// e.g.  for [0xBE]:
+// [1010 1101]
+// firmatified = [ 0010 1101, 0000 0001 ]
+//             = [ 0x2D, 0x01]
+inline std::vector<uint8_t> firmatify_8(const uint8_t val) {
+    return std::vector<uint8_t>(val & 0x7F, (val & 0x80) >> 7);
+}
 
 // Decode Firmata 7-bit packets into a 32-bit integer
 // See firmatify_32 for an explanation of what Firmata does to packets
@@ -91,14 +105,37 @@ inline uint32_t decode_32(const std::vector<uint8_t>::const_iterator& data) {
            | (data[4] | data[5] << 7) << 16
            | (data[6] | data[7] << 7) << 24;
 }
+
 inline uint32_t decode_32(const std::vector<uint8_t>& data) { return decode_32(data.cbegin()); }
 
 // Decode Firmata 7-bit packets into a 16-bit integer
 // See firmatify_16 for an explanation of what Firmata does to packets
-inline uint16_t decode_16(const std::vector<uint8_t>::const_iterator & data) {
+inline uint16_t decode_16(const std::vector<uint8_t>::const_iterator& data) {
     return data[0] | data[1] << 7
            | (data[2] | data[3] << 7) << 8;
 }
+
 inline uint16_t decode_16(const std::vector<uint8_t>& data) { return decode_16(data.cbegin()); }
+
+// Decode Firmata 7-bit packets into an 8-bit integer
+// See firmatify_8 for an explanation of what Firmata does to packets
+inline uint16_t decode_8(const std::vector<uint8_t>::const_iterator& data) { return data[0] | data[1] << 7; }
+
+inline uint8_t decode_8(const std::vector<uint8_t>& data) { return decode_8(data.cbegin()); }
+
+// Decode a Firmata 7-bit packet stream into an std::string
+std::string decode_string(const std::vector<uint8_t>& data) {
+    std::string s;
+    if (data.size() <= 1) { return s; } // Need at least 2 elements to do anything
+
+    // Since we consume 2 elements at a time, we need to iterate until the second-last element
+    // Note: data.cend() - 1 is safe because we know data.size() >= 2
+    std::vector<uint8_t>::const_iterator end = data.cend() - 1;
+    for (std::vector<uint8_t>::const_iterator i = data.cbegin(); i != end; i += 2) {
+        s.append(static_cast<char>(decode_8(i)));
+    }
+
+    return s;
+}
 
 #endif //COMMUNICATION_MASTER_EXAMPLE_UTILS_H
