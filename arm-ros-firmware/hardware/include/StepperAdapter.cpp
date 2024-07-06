1c21c31bb6f21cd46f03846522ec3773ed72a1c0
Noah Reeder <noahreederatc@gmail.com>
2024-07-05 20:58:04 -0500

    Added logging initialization to StepperAdapter

--- arm-ros-firmware/hardware/include/StepperAdapter.cpp
+++ arm-ros-firmware/hardware/include/StepperAdapter.cpp
@@ -3,8 +3,25 @@
 //
 
 #include "StepperAdapter.hpp"
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
+#include <boost/log/utility/setup/file.hpp>
+
+constexpr boost::log::trivial::severity_level LOG_LEVEL = boost::log::trivial::debug;
+constexpr uint32_t TOTAL_LOG_SIZE = 100 * 1024 * 1024; // 100 MiB
 
 void StepperAdapter::init(std::size_t NUM_JOINTS) {
+    // Setup logging
+    boost::log::add_file_log(
+            boost::log::keywords::file_name = "[%TimeStamp%]_%N.log",
+            boost::log::keywords::rotation_size = TOTAL_LOG_SIZE,
+            boost::log::keywords::format = "[%TimeStamp%]: %Message%",
+            boost::log::keywords::auto_flush = true
+    );
+    boost::log::core::get()->set_filter(boost::log::trivial::severity >= LOG_LEVEL);
+    boost::log::add_common_attributes();
+    BOOST_LOG_TRIVIAL(debug) << "Logging started";
+
     this->positions.resize(NUM_JOINTS);
     this->velocities.resize(NUM_JOINTS);
     this->commands.resize(NUM_JOINTS);
