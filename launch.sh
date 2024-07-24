1076cb2f91022ef433227d721a505b27dd444684
Noah Reeder <noahreederatc@gmail.com>
2024-07-24 01:26:41 -0500

    Added files to create systemd service

---
+++ launch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Start and then close a Pyfirmata2 connection
+# Don't ask me why, but if we don't do this after the Arduino is plugged in, then the Firmata connection code from
+# openFrameworks doesn't work...
+python -c "import pyfirmata2; pyfirmata2.Arduino('/dev/umrt-arm').exit()"
+
+docker run \
+  --rm \
+  --name umrt-arm \
+  --device=/dev/umrt-arm \
+  -p 50000:22 \
+  -v /etc/arm-firmware:/releases/arm-firmware \
+  --entrypoint /bin/bash \
+  umrt-arm-image \
+  -c "cd /releases/arm-firmware/arm-ros-firmware && source install/setup.bash && ros2 launch arm_ros_firmware diffbot.launch.py"
