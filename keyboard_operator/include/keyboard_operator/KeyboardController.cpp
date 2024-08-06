25caaf5f890548d8acad4ef004e26a5138050b0b
Noah Reeder <noahreederatc@gmail.com>
2024-08-05 20:41:50 -0500

    Created KeyboardController class in keyboard_operator

---
+++ keyboard_operator/include/keyboard_operator/KeyboardController.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Noah on 2024-08-05.
+//
+
+#include "KeyboardController.h"
---
+++ keyboard_operator/include/keyboard_operator/KeyboardController.h
@@ -0,0 +1,17 @@
+//
+// Created by Noah Reeder on 2024-08-05.
+//
+
+#ifndef KEYBOARD_OPERATOR_KEYBOARDCONTROLLER_H
+#define KEYBOARD_OPERATOR_KEYBOARDCONTROLLER_H
+
+#include "rclcpp/rclcpp.hpp"
+
+
+class KeyboardController : public rclcpp::Node {
+public:
+    KeyboardController();
+};
+
+
+#endif //KEYBOARD_OPERATOR_KEYBOARDCONTROLLER_H
