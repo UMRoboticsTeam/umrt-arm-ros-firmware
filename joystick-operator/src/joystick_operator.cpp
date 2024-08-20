e7537cd9a8e84f25633178259f8ba0f9e1c21e4c
Noah Reeder <noahreederatc@gmail.com>
2024-08-19 21:13:45 -0500

    Added main to joystick_operator

--- joystick-operator/src/joystick_operator.cpp
+++ joystick-operator/src/joystick_operator.cpp
@@ -1,3 +1,13 @@
 //
 // Created by Noah on 2024-08-18.
 //
+
+#include "rclcpp/rclcpp.hpp"
+#include "joystick_operator/joystick_teleop_node.hpp"
+
+int main(int argc, char* argv[]){
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<JoystickTeleopNode>());
+    rclcpp::shutdown();
+    return 0;
+}
