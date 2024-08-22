4e866110dc52e6555ff0d52283fbb30e66c42860
Noah Reeder <noahreederatc@gmail.com>
2024-08-21 20:58:29 -0500

    Added Doxygen documentation to ConnectionManager.hpp and cleaned up ConnectionManager.cpp

--- arduino-stepper-controller/ConnectionManager.cpp
+++ arduino-stepper-controller/ConnectionManager.cpp
@@ -1,86 +1,59 @@
-// This example shows basic use of a Pololu High Power Stepper Motor Driver.
-//
-// It shows how to initialize the driver, configure various settings, and enable
-// the driver.  It shows how to send pulses to the STEP pin to step the motor
-// and how to switch directions using the DIR pin.
-//
-// Before using this example, be sure to change the setCurrentMilliamps36v4 line
-// to have an appropriate current limit for your system.  Also, see this
-// library's documentation for information about how to connect the driver:
-//   http://pololu.github.io/high-power-stepper-driver
-
-#include <SPI.h>
 #include "ConnectionManager.hpp"
+#include <SPI.h>
 
-// This period is the length of the delay between steps, which controls the
-// stepper motor's speed.  You can increase the delay to make the stepper motor
-// go slower.  If you decrease the delay, the stepper motor will go faster, but
-// there is a limit to how fast it can go before it starts missing steps.
-const uint16_t StepPeriodUs = 2000;
-
-static void ConnectionManager::init()
-{
-  SPI.begin();
+static void ConnectionManager::init() {
+    SPI.begin();
 }
 
-uint8_t ConnectionManager::add_driver(uint8_t step_pin, uint8_t dir_pin, uint8_t cs_pin, uint16_t current, HPSDStepMode step_mode){
-  // Create the HighPowerStepperDriver object, and save it and its info to the arrays
-  HighPowerStepperDriver* sd = new HighPowerStepperDriver();
-  this->drivers[this->num_drivers] = sd;
-  this->step_pins[this->num_drivers] = step_pin;
-  this->dir_pins[this->num_drivers] = dir_pin;
-  
-  sd->setChipSelectPin(cs_pin);
+uint8_t ConnectionManager::add_driver(uint8_t step_pin, uint8_t dir_pin, uint8_t cs_pin, uint16_t current, HPSDStepMode step_mode) {
+    // Create the HighPowerStepperDriver object, and save it and its info to the arrays
+    HighPowerStepperDriver* sd = new HighPowerStepperDriver();
+    this->drivers[this->num_drivers] = sd;
+    this->step_pins[this->num_drivers] = step_pin;
+    this->dir_pins[this->num_drivers] = dir_pin;
 
-  // Drive the STEP and DIR pins low initially.
-  pinMode(step_pin, OUTPUT);
-  digitalWrite(step_pin, LOW);
-  pinMode(dir_pin, OUTPUT);
-  digitalWrite(dir_pin, LOW);
+    sd->setChipSelectPin(cs_pin);
 
-  // Give the driver some time to power up.
-  delay(1);
+    // Drive the STEP and DIR pins low initially.
+    pinMode(step_pin, OUTPUT);
+    digitalWrite(step_pin, LOW);
+    pinMode(dir_pin, OUTPUT);
+    digitalWrite(dir_pin, LOW);
 
-  // TODO: Might want to wait until non-zero read from driver SPI connection
+    // Give the driver some time to power up.
+    delay(1);
 
-  // Reset the driver to its default settings and clear latched status
-  // conditions.
-  sd->resetSettings();
-  sd->clearStatus();
+    // TODO: Might want to wait until non-zero read from driver SPI connection
 
-  // Select auto mixed decay.  TI's DRV8711 documentation recommends this mode
-  // for most applications, and we find that it usually works well.
-  sd->setDecayMode(HPSDDecayMode::AutoMixed);
+    // Reset the driver to its default settings and clear latched status
+    // conditions.
+    sd->resetSettings();
+    sd->clearStatus();
 
-  // Set the current limit. You should change the number here to an appropriate
-  // value for your particular system.
-  sd->setCurrentMilliamps36v4(current);
+    // Set driver settings and enable it
+    sd->setDecayMode(HPSDDecayMode::AutoMixed); // As recommended by HighPowerStepperMotorDriver example
+    sd->setCurrentMilliamps36v4(current);
+    sd->setStepMode(step_mode);
+    sd->enableDriver();
 
-  // Set the number of microsteps that correspond to one full step.
-  sd->setStepMode(step_mode);
-
-  // Enable the motor outputs.
-  sd->enableDriver();
-
-  return this->num_drivers++;
+    return this->num_drivers++;
 }
 
-HighPowerStepperDriver* ConnectionManager::get_driver(uint8_t index){
-  return index < this->num_drivers ? this->drivers[index] : NULL;
+HighPowerStepperDriver* ConnectionManager::get_driver(uint8_t index) {
+    return index < this->num_drivers ? this->drivers[index] : NULL;
 }
 
-uint8_t ConnectionManager::get_step_pin(uint8_t index){
-  return index < this->num_drivers ? this->step_pins[index] : NULL;
+uint8_t ConnectionManager::get_step_pin(uint8_t index) {
+    return index < this->num_drivers ? this->step_pins[index] : NULL;
 }
 
-uint8_t ConnectionManager::get_dir_pin(uint8_t index){
-  return index < this->num_drivers ? this->dir_pins[index] : NULL;
+uint8_t ConnectionManager::get_dir_pin(uint8_t index) {
+    return index < this->num_drivers ? this->dir_pins[index] : NULL;
 }
 
-ConnectionManager::~ConnectionManager(){
-  for (int i = 0; i < this->num_drivers; ++i)
-  {
-    delete this->drivers[i];
-  }
-  this->num_drivers = 0;
+ConnectionManager::~ConnectionManager() {
+    for (int i = 0; i < this->num_drivers; ++i) {
+        delete this->drivers[i];
+    }
+    this->num_drivers = 0;
 }
--- arduino-stepper-controller/ConnectionManager.hpp
+++ arduino-stepper-controller/ConnectionManager.hpp
@@ -2,7 +2,7 @@
 #define CONNECTION_MANAGER_HPP
 
 // Pololu High-Power Stepper Motor Driver Library for Arduino
-// https://github.com/pololu/high-power-stepper-driver-arduino/tree/master
+// https://github.com/pololu/high-power-stepper-driver-arduino/
 // For interacting with https://www.pololu.com/product/3730
 #include <HighPowerStepperDriver.h>
 
@@ -10,23 +10,73 @@
 
 class ConnectionManager {
 public:
-  static void init();
+    /**
+     * Initializes the required Arduino modules for ConnectionManager operation. Must be called before the
+     * construction of a ConnectionManager.
+     */
+    static void init();
 
-  uint8_t add_driver(uint8_t step_pin, uint8_t dir_pin, uint8_t cs_pin, uint16_t current, HPSDStepMode step_mode);
+    /**
+     * Creates a @ref HighPowerStepperDriver to be managed by this ConnectionManager.
+     *
+     * @param step_pin the Arduino pin the driver's STEP pin is connected to
+     * @param dir_pin the Arduino pin the driver's DIR pin is connected to
+     * @param cs_pin the Arduino pin the driver's SCS pin is connected to
+     * @param current the maximum current this driver is allowed to supply to the connected motor
+     * @param step_mode the microstepping mode to use with this driver
+     * @return the index assigned to this driver
+     */
+    uint8_t add_driver(uint8_t step_pin, uint8_t dir_pin, uint8_t cs_pin, uint16_t current, HPSDStepMode step_mode);
 
-  HighPowerStepperDriver* get_driver(uint8_t index);
+    /**
+     * Retrieves a pointer to the @ref HighPowerStepperDriver at the specified index.
+     *
+     * @param index index of the requested driver
+     * @return a pointer to the @ref HighPowerStepperDriver, or `NULL`
+     */
+    HighPowerStepperDriver* get_driver(uint8_t index);
 
-  uint8_t get_step_pin(uint8_t index);
+    /**
+     * Retrieves the Arduino pin associated with the specified driver's STEP pin.
+     *
+     * @param index index of the driver to examine
+     * @return the Arduino pin the driver's STEP pin is connected to
+     */
+    uint8_t get_step_pin(uint8_t index);
 
-  uint8_t get_dir_pin(uint8_t index);
+    /**
+     * Retrieves the Arduino pin associated with the specified driver's DIR pin.
+     *
+     * @param index index of the driver to examine
+     * @return the Arduino pin the driver's DIR pin is connected to
+     */
+    uint8_t get_dir_pin(uint8_t index);
 
-  ~ConnectionManager();
+    /**
+     * Destroys this ConnectionManager and associated @ref HighPowerStepperDrivers.
+     */
+    ~ConnectionManager();
 
 private:
-  HighPowerStepperDriver* drivers[MAX_DRIVERS];
-  uint8_t step_pins[MAX_DRIVERS];
-  uint8_t dir_pins[MAX_DRIVERS];
-  uint8_t num_drivers = 0;
+    /**
+     * Array holding pointers to dynamically-allocated @ref HighPowerStepperDrivers.
+     */
+    HighPowerStepperDriver* drivers[MAX_DRIVERS];
+
+    /**
+     * The STEP pin associated with the co-indexed @ref HighPowerStepperDriver stored in @ref drivers
+     */
+    uint8_t step_pins[MAX_DRIVERS];
+
+    /**
+     * The DIR pin associated with the co-indexed @ref HighPowerStepperDriver stored in @ref drivers
+     */
+    uint8_t dir_pins[MAX_DRIVERS];
+
+    /**
+     * The number of @ref HighPowerStepperDriver%s stored in @ref drivers
+     */
+    uint8_t num_drivers = 0;
 };
 
 #endif
